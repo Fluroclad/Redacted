9fc69ed37bc070ee8908fb5f3ed50af8739dded2
Daniel Dickson <fluroclad@gmail.com>
2020-08-28 10:53:25 +0100

    Added Authentication to Blizzard API

--- bot/redacted.py
+++ bot/redacted.py
@@ -3,6 +3,8 @@ import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from wow import *
+
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -12,5 +14,30 @@ bot = commands.Bot(command_prefix = "!r-")
 async def test_com(ctx):
     response = "This is a testing command has been updated."
     await ctx.send(response)
+
+@bot.command( name = "curve", help = "Returns those who have curve and don't for current raid tier.")
+async def curve(ctx):
+    response = "Return Curve. (Maybe add curve roll to those who have obtained it)"
+    await ctx.send(response)
+
+@bot.command( name = "ce", help = "Returns those who have cutting edge and don't for current raid tier.")
+async def cutting_edge(ctx):
+    response = "lol u think we ever getting cutting edge."
+    await ctx.send(response)
+
+@bot.command( name = "update-characters", help = "Update all characters attached to your discord account.")
+async def update_characters(ctx):
+    response = "Update main and alt characters."
+    await ctx.send(response)
+
+@bot.command( name = "get-character", help = "Add WoW charcter. Usage: !r-add-character <CharacterName>")
+async def get_character(ctx, character_name: str):
+
+    character = await get_character_info(character_name.lower())
+    
+    if character == "not_found":
+        await ctx.send("Character not found!")
+
+    await ctx.send(character)
 
 bot.run(TOKEN)
---
+++ bot/wow.py
@@ -0,0 +1,77 @@
+import os
+import aiohttp
+
+from dotenv import load_dotenv
+load_dotenv()
+WOW_ID =  os.getenv("WOW_CLIENT_ID")
+WOW_SECRET = os.getenv("WOW_CLIENT_SECRET")
+
+# Fetch OAuth Token
+async def get_oauth_token():
+    auth_path = "https://eu.battle.net/oauth/token"
+
+    auth = aiohttp.BasicAuth(
+        login = WOW_ID,
+        password = WOW_SECRET
+    )
+
+    try:
+        async with aiohttp.ClientSession(auth=auth) as client:
+            async with client.get(
+                auth_path, params={"grant_type": "client_credentials"}
+            ) as auth_response:
+                assert auth_response.status == 200
+                json = await auth_response.json()
+                return json["access_token"]
+
+    except Exception as error:
+        return "error_auth"
+
+# Fetch character data from WoW API
+async def get_data(auth_token, name):
+    base_api_path = "https://eu.api.blizzard.com"
+    
+    try:
+        async with aiohttp.ClientSession() as client:
+            api_path = ( "%s/profile/wow/character/silvermoon/%s?namespace=profile-eu&locale=en_GB&access_token=%s" 
+                        % (base_api_path, name, auth_token) )
+            
+            #print(api_path, flush = True)
+            
+            async with client.get(
+                api_path
+            ) as api_response:
+                print(api_response, flush = True)
+                if api_response.status == 200:
+                    
+                    api_json = await api_response.json()
+                    return api_json
+                
+                elif api_response.status == 404:
+                    print("Character is not found")
+                    return "not_found"
+
+    except Exception as error:
+        print(error)
+        print("Error: Connection error when retrieving game data.")
+        return "connection_error"
+
+# Get character info 
+async def get_character_info(name):
+    auth_token = await get_oauth_token()
+
+    info = await get_data(auth_token, name)
+
+    if info == "not_found":
+        return info
+
+    else:
+        character_sheet = {
+            "name": info["name"],
+            "level": info["level"],
+            "class": info["character_class"]["name"],
+            "spec": info["active_spec"]["name"]
+        }
+
+    
+    return character_sheet
